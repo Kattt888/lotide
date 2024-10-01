f42048020974ddfa17f8d99c17a0d6c64c44f888
Katrina <katrinabyrd88@gmail.com>
2024-10-01 01:23:40 -0400

    flatten complete

---
+++ flatten.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+      return true;
+  };
+
+//assertArrayEqual function
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) {
+    console.log(`\u{1F603} Assertion Passed:${arr1} === ${arr2}`);
+  } else {
+    console.log(`\u{1F62E} Assertion Failed:${arr1} !== ${arr2}`);
+  }
+};
+
+//flatten
+const flatten = function(array) {
+  let flattenArray = [];
+
+  //loop
+for (let i = 0; i < array.length; i++) {
+  if (Array.isArray(array[i])) {
+    flattenArray = flattenArray.concat(array[i]);
+  } else {
+    flattenArray.push(array[i]);
+  }
+}
+return flattenArray;
+
+};
+
+
+// Outputs
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
