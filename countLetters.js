8feb3b2c2102f5a5deb2d3d890f430ec71979030
Katrina <katrinabyrd88@gmail.com>
2024-10-03 02:21:29 -0400

    updated answer as per feedback

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,10 @@
-//const assertEqual = function(actual, expected) {
- // if (actual === expected) {
- //   console.log(`\u{1F603} Assertion Passed:${actual} === ${expected}`);
-//  } else {
- //   console.log(`\u{1F62E} Assertion Failed:${actual} !== ${expected}`);
- // }
-//};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F603} Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1F62E} Assertion Failed:${actual} !== ${expected}`);
+  }
+};
 
 const countLetters = function(sentence) {
   const results = {}
@@ -28,3 +28,21 @@ return results;
 //Test
 const result = countLetters("halloween is the best time of the year");
 console.log(result);
+
+
+//assertEqual function
+assertEqual(result["h"], 3); // Pass
+assertEqual(result["a"], 2); // Pass
+assertEqual(result["e"], 7); // Pass
+assertEqual(result["t"], 4); // Pass
+assertEqual(result["o"], 2); // Pass
+assertEqual(result["s"], 2); // Pass
+assertEqual(result["y"], 1); // Pass
+assertEqual(result["r"], 1); // Pass
+assertEqual(result["l"], 2); // Pass
+assertEqual(result["n"], 1); // Pass
+assertEqual(result["w"], 1); // Pass
+assertEqual(result["i"], 2); // Pass
+assertEqual(result["b"], 1); // Pass
+assertEqual(result["m"], 1); // Pass
+assertEqual(result["f"], 1);// Pass
