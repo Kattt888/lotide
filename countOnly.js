74bd23ab3e38ccff57cdc6a824e10486ddab01c1
Katrina <katrinabyrd88@gmail.com>
2024-10-03 00:50:57 -0400

    updated as per ai feedback

--- countOnly.js
+++ countOnly.js
@@ -5,13 +5,17 @@ const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
   // Loop 
+for (const item of allItems) {
+
   if (itemsToCount[item]) {
     if (results[item]) {
       results[item] += 1;
     } else {
       results[item] = 1;
+  
     }
   }
+}
 
   return results;
 };
