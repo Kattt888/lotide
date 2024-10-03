f306285d14668f510162761884b4fdc656b2ce96
Katrina <katrinabyrd88@gmail.com>
2024-10-03 00:45:27 -0400

    countOnly completed

---
+++ countOnly.js
@@ -0,0 +1,37 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  // Loop 
+  if (itemsToCount[item]) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+// Call the function
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
