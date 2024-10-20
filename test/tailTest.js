ea3faaebbb64aa82e9f2ffafa35c8ab7dd6cba3a
Katrina <katrinabyrd88@gmail.com>
2024-10-19 23:44:40 -0400

    corrected

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,8 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assertArraysEqual = require("../assertArraysEqual");
 
 
 // TEST
-assertEqual(tail([1, 2, 3]), [2, 3]); // should pass
-assertEqual(tail([1]), []); // should pass
-assertEqual(tail([]), []); // should pass
-
-// Log the results for confirmation
-console.log(tail([1, 2, 3])); // should log [2, 3]
-console.log(tail([1]));        // should log []
-console.log(tail([]));         // should log []
+assertArraysEqual(tail([1, 2, 3]), [2, 3]); // should pass
+assertArraysEqual(tail([1]), []); // should pass
+assertArraysEqual(tail([]), []); // should pass
