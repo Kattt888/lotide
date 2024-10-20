0c7e220d9eef06f6553613d716865dbae9a3f9c5
Katrina <katrinabyrd88@gmail.com>
2024-10-19 20:54:08 -0400

    completed lotide refactor example

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+// test/assertEqualTest.js
+
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); // should fail
+assertEqual(1, 1); // should pass
---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F603} Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1F62E} Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertEqual;
