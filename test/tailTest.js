f0a1ac8bfe93bd0d81be19fbdad97f328b40afc4
Katrina <katrinabyrd88@gmail.com>
2024-10-19 23:56:57 -0400

    update test

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,4 +5,10 @@ const assertEqual = require("../assertEqual");
 // TEST
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
-assertEqual(words.length, 3);
+assertEqual(words.length, 3); 
+
+const result = tail(["Hello", "Lighthouse", "Labs"]); 
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const emptyArray = [];
+assertEqual(tail(emptyArray), []); 
